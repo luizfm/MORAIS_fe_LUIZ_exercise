51c9113dbbb7eb8058239687ed7720a9566bdb04
luizfm <luiz.ado@hotmail.com>
2023-07-14 20:17:41 -0300

    feat: add react-query devtools to application

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import * as React from 'react';
 import {RouterProvider} from 'react-router-dom';
 import {QueryClient, QueryClientProvider} from 'react-query';
 import {routes} from 'routes';
+import {ReactQueryDevtools} from 'react-query/devtools';
 
 const client = new QueryClient();
 
 const App = () => {
-  return (
-    <QueryClientProvider client={client}>
-      <RouterProvider router={routes} />
-    </QueryClientProvider>
-  );
+    return (
+        <QueryClientProvider client={client}>
+            <RouterProvider router={routes} />
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+    );
 };
 
 export default App;
